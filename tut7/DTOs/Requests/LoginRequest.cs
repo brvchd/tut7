165f09bc173fa300eb94c14b9a7d1f0e66f824dd
Dmytro Borovych <s18963@pjwstk.edu.pl>
2020-05-08 12:22:03 +0200

    start

---
+++ tut7/DTOs/Requests/LoginRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tut7.DTOs.Requests
+{
+    public class LoginRequest
+    {
+    }
+}

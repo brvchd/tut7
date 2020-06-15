45337e09d7dd7ca7ccc414400baf7d4de6bba7ae
Dmytro Borovych <s18963@pjwstk.edu.pl>
2020-06-15 20:15:58 +0200

    Costemic changes

--- tut7/DTOs/Responce/PromoteStudentResponse.cs
+++ tut7/DTOs/Responce/PromoteStudentResponse.cs
@@ -4,9 +4,9 @@ namespace tut7.DTOs.Responce
 {
     public class PromoteStudentResponse
     {
-        [Required(ErrorMessage = "Provide name of study")]
+        
         public string Name { get; set; }
-        [Required(ErrorMessage = "Provide semester")]
+        
         public int Semester { get; set; }
     }
 }
